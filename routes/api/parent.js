dc499809c981f3ee908f4f2e4ddef594bc23b07d
Anthony-Rondina <anthony.rondina@icloud.com>
2022-07-23 14:23:40 -0400

    added puppy and parent route controller

---
+++ routes/api/parent.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const parentCtrl = require('../../controllers/api/parentCtrl');
+
+router.get('/', parentCtrl.get)
+
+router.put('/:id', parentCtrl.put)
+
+router.post('/', parentCtrl.create);
+
+router.delete('/:id', parentCtrl.destroy);
+
+router.get(`/chosenart/:id`, parentCtrl.show);
+
+module.exports = router;
---
+++ routes/api/puppy.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const puppyCtrl = require('../../controllers/api/puppyCtrl');
+
+router.get('/', puppyCtrl.get)
+
+router.get('/:artType', puppyCtrl.getAllFilteredArt)
+
+router.put('/:id', puppyCtrl.put)
+
+router.post('/', puppyCtrl.create);
+
+router.delete('/:id', puppyCtrl.destroy);
+
+router.get(`/chosenart/:id`, puppyCtrl.show);
+
+module.exports = router;
