82412ada7ccfd6bcd54a8d9e5e71edc66137e326
Anthony-Rondina <anthony.rondina@icloud.com>
2022-07-23 14:24:20 -0400

    added litter route controller

---
+++ routes/api/litter.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const litterCtrl = require('../../controllers/api/litterCtrl');
+
+router.get('/', litterCtrl.get)
+
+router.put('/:id', litterCtrl.put)
+
+router.post('/', litterCtrl.create);
+
+router.delete('/:id', litterCtrl.destroy);
+
+router.get(`/chosenart/:id`, litterCtrl.show);
+
+module.exports = router;
