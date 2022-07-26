1d2b8abff6e5c51de2a8455ea8101fcbb8eb7f3c
Anthony-Rondina <anthony.rondina@icloud.com>
2022-07-26 13:00:06 -0400

    starting work on creating a parent page

--- src/pages/Parents/CreateParent/CreateParent.js
+++ src/pages/Parents/CreateParent/CreateParent.js
@@ -1,8 +1,24 @@
 import axios from "axios"
 import { useState, useEffect } from "react"
+import { useNavigate } from "react-router-dom";
+import { useRef } from "react"
 
 const CreateParent = () => {
+    const [parents, setParents] = useState({})
+    useEffect(() => {
+        (async () => {
+            try {
 
+                setLoading(true)
+                const response = await axios.get(`/api/cards/`)
+                setParents(response.data)
+                setLoading(false)
+
+            } catch (err) {
+                console.log(`err`)
+            }
+        })()
+    }, [])
 
     return (
         <div>CreateParent</div>
