d9b5015a81f295b7841c129201744bd6bd5b5336
Anthony-Rondina <anthony.rondina@icloud.com>
2022-07-30 10:57:32 -0400

    working on create litter page

--- src/pages/Litters/CreateLitter/CreateLitter.js
+++ src/pages/Litters/CreateLitter/CreateLitter.js
@@ -1,9 +1,15 @@
 import { useNavigate } from "react-router-dom"
 import { useEffect, useState, useRef } from "react"
 import axios from "axios"
-
-
 const CreateLitter = () => {
+    const name = useRef()
+    const father = useRef()
+    const mother = useRef()
+    const bio = useRef()
+    const splashImg = useRef()
+    const imgs = useRef()
+    const videos = useRef()
+
     return (
         <div>CreateLitter</div>
     )
