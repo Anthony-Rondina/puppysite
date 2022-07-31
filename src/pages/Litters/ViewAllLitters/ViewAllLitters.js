fdd4f8dc0728a5b795ec38866ebd63ddcb300380
Anthony-Rondina <anthony.rondina@icloud.com>
2022-07-31 16:52:10 -0400

    created view all litters page

--- src/pages/Litters/ViewAllLitters/ViewAllLitters.js
+++ src/pages/Litters/ViewAllLitters/ViewAllLitters.js
@@ -1,6 +1,39 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+import { Link } from "react-router-dom"
 const ViewAllLitters = () => {
+    const [litters, setLitters] = useState([])
+    const [loading, setLoading] = useState(false)
+    useEffect(() => {
+        (async () => {
+            try {
+                setLoading(true)
+                const response = await axios.get(`/api/litters/`)
+                console.log("response is", response.data)
+                setLitters(response.data)
+                setLoading(false)
+            } catch (err) {
+                console.log(err)
+            }
+        })()
+    }, [])
+    const loaded = () => {
+        return (
+            <>
+                {litters.map((litter) => {
+                    return (
+                        <Link to={`/viewlitter/${litter._id}`}><h2>{litter.name}</h2></Link>
+                    )
+                })}
+            </>
+        )
+    }
+    const waiting = () => {
+        return <h1>Loading</h1>
+    }
+
     return (
-        <div>ViewAllLitters</div>
+        !loading ? loaded() : waiting()
     )
 }
 export default ViewAllLitters
--- src/pages/Litters/ViewLitter/ViewLitters.js
+++ src/pages/Litters/ViewLitter/ViewLitters.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react"
+
 const ViewLitters = () => {
     return (
         <div>ViewLitters</div>
