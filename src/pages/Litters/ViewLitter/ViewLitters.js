30accab4c76609430dc6c80cb13d0733197ac5ac
Anthony-Rondina <anthony.rondina@icloud.com>
2022-07-31 16:59:37 -0400

    created view all litters page

--- src/pages/Litters/ViewLitter/ViewLitters.js
+++ src/pages/Litters/ViewLitter/ViewLitters.js
@@ -1,8 +1,36 @@
 import { useState, useEffect } from "react"
-
+import axios from "axios"
+import { useParams } from "react-router-dom"
 const ViewLitters = () => {
+    const { id } = useParams()
+    const [loading, setLoading] = useState(false)
+    const [litter, setLitter] = useState({})
+    useEffect(() => {
+        (async () => {
+            try {
+                setLoading(true)
+                const response = await axios.get(`/api/litters/${id}`)
+                console.log("response is", response.data)
+                setLitter(response.data)
+                setLoading(false)
+            } catch (err) {
+                console.log(err)
+            }
+        })()
+    }, [])
+    const loaded = () => {
+        return (
+            <>
+                <h1>{litter.name}</h1>
+            </>
+        )
+    }
+    const waiting = () => {
+        return <h1>Loading</h1>
+    }
+
     return (
-        <div>ViewLitters</div>
+        !loading ? loaded() : waiting()
     )
 }
 export default ViewLitters
