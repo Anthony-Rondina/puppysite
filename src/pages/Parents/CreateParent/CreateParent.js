0a271004246904ad831792bc3682cefd7dcb6df1
Anthony-Rondina <anthony.rondina@icloud.com>
2022-08-15 09:49:51 -0400

    styling create parent form

--- src/pages/Parents/CreateParent/CreateParent.js
+++ src/pages/Parents/CreateParent/CreateParent.js
@@ -55,53 +55,58 @@ const CreateParent = () => {
 
             <Container style={{
                 maxWidth: "800px", display: "flex", flexDirection: "column", justifyContent: 'center', alignItems: 'center',
-                height: "90vh",
+                height: "100%",
                 overflow: 'hidden',
                 backgroundColor: "tan"
             }}>
-                <img style={{ width: "800px", borderTop: "5px black solid", borderBottom: "5px black solid" }} src="https://media.kidadl.com/21_Pawfect_Facts_About_The_Doberman_Dog_Kids_Will_Love_d779805fb7.jpg" alt="" />
-                <h1 className="mt-5">Create New Parent</h1>
-                <a href="/parents"><Button className="mb-3" variant="secondary">Back to Parents</Button></a>
-                <Form onSubmit={handleSubmit}>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Enter Name of Parent</Form.Label>
-                        <Form.Control type="text" placeholder="Enter Name" ref={name} />
-                    </Form.Group>
+                <img style={{ width: "100%", borderTop: "5px black solid", borderBottom: "5px black solid" }} src="https://media.kidadl.com/21_Pawfect_Facts_About_The_Doberman_Dog_Kids_Will_Love_d779805fb7.jpg" alt="" />
+                <div style={{ display: "flex", flexDirection: "column", justifyContent: 'space-around', alignItems: 'center' }}>
 
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Gender:</Form.Label>
-                        <Form.Select aria-label="Default select example" ref={gender}>
-                            <option value="true">Male</option>
-                            <option value="false">Female</option>
-                        </Form.Select>
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                        <Form.Label>Enter Bio of Parent</Form.Label>
-                        <Form.Control placeholder="Enter Bio" ref={bio} as="textarea" rows={3} />
-                    </Form.Group>
-                    <Form.Label>Upload Splash Image</Form.Label>
-                    <Form.Group style={{ display: 'flex' }} className="mb-3" controlId="exampleForm.fileUpload">
-                        <Form.Control
-                            type="file"
-                            required
-                            name="file"
-                            onChange={handleFiles}
-                            ref={image}
-                        />
-                        <Button variant={!submitButton ? "warning" : "success"} style={{ cursor: "pointer", }} type='button' onClick={upload}>{body.img ? "Image Uploaded" : "Upload Image"}</Button>
-                    </Form.Group>
+                    <h1 className="mt-5">Create New Parent</h1>
+                    <a href="/parents"><Button className="mb-3" variant="secondary">Back to Parents</Button></a>
+                </div>
+                <div>
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Enter Name of Parent</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Name" ref={name} />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Gender:</Form.Label>
+                            <Form.Select aria-label="Default select example" ref={gender}>
+                                <option value="true">Male</option>
+                                <option value="false">Female</option>
+                            </Form.Select>
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                            <Form.Label>Enter Bio of Parent</Form.Label>
+                            <Form.Control placeholder="Enter Bio" ref={bio} as="textarea" rows={3} />
+                        </Form.Group>
+                        <Form.Label>Upload Splash Image</Form.Label>
+                        <Form.Group style={{ display: 'flex' }} className="mb-3" controlId="exampleForm.fileUpload">
+                            <Form.Control
+                                type="file"
+                                required
+                                name="file"
+                                onChange={handleFiles}
+                                ref={image}
+                            />
+                            <Button variant={!submitButton ? "warning" : "success"} style={{ cursor: "pointer", }} type='button' onClick={upload}>{body.img ? "Image Uploaded" : "Upload Image"}</Button>
+                        </Form.Group>
 
 
-                    <Form.Text className="text-muted">
-                        You must submit a photo.
-                    </Form.Text>
-                    <Form.Group className="mb-3 mt-3" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Is the Parent Retired?" ref={retired} />
-                    </Form.Group>
-                    <Button variant="success" type="submit">
-                        Create New Parent
-                    </Button>
-                </Form>
+                        <Form.Text className="text-muted">
+                            You must submit a photo.
+                        </Form.Text>
+                        <Form.Group className="mb-3 mt-3" controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Is the Parent Retired?" ref={retired} />
+                        </Form.Group>
+                        <Button variant="success" type="submit">
+                            Create New Parent
+                        </Button>
+                    </Form>
+                </div>
             </Container>
         </>
     )
