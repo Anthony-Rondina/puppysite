6ce974267ee951fb76f24f1b37cb2d9f34e1a80e
Anthony-Rondina <anthony.rondina@icloud.com>
2022-08-17 23:12:29 -0400

    styled parent page

--- src/pages/Litters/CreateLitter/CreateLitter.js
+++ src/pages/Litters/CreateLitter/CreateLitter.js
@@ -62,82 +62,85 @@ const CreateLitter = () => {
     const loaded = () => {
         return (
             <>
-                <>
 
-                    <Container style={{
-                        maxWidth: "800px", display: "flex", flexDirection: "column", justifyContent: 'center', alignItems: 'center',
-                        height: "100%",
-                        overflow: 'hidden',
-                        backgroundColor: "tan"
-                    }}>
-                        <img style={{ width: "100%", borderTop: "5px black solid", borderBottom: "5px black solid" }} src="https://www.bubblypet.com/wp-content/uploads/2021/05/Four-black-and-tan-Doberman-puppies-for-sale.jpg" alt="" />
-                        <div style={{ display: "flex", flexDirection: "column", justifyContent: 'space-around', alignItems: 'center' }}>
+                <Container style={{
+                    maxWidth: "800px", display: "flex", flexDirection: "column", justifyContent: 'center', alignItems: 'center',
+                    height: "100%",
+                    overflow: 'hidden',
+                    backgroundColor: "tan"
+                }}>
+                    <img style={{ width: "100%", borderTop: "5px black solid", borderBottom: "5px black solid" }} src="https://www.bubblypet.com/wp-content/uploads/2021/05/Four-black-and-tan-Doberman-puppies-for-sale.jpg" alt="" />
+                    <div style={{ display: "flex", flexDirection: "column", justifyContent: 'space-around', alignItems: 'center' }}>
 
-                            <h1 className="mt-5">Create New Litter</h1>
-                            <a href="/parents"><Button className="mb-3" variant="secondary">Back to Litters</Button></a>
-                        </div>
-                        <div>
-                            <Form onSubmit={handleSubmit}>
-                                <Form.Group className="mb-3" controlId="formBasicEmail">
-                                    <Form.Label>Enter Name of Litter</Form.Label>
-                                    <Form.Control type="text" placeholder="Enter Name" ref={name} />
-                                </Form.Group>
+                        <h1 className="mt-5">Create New Litter</h1>
+                        <a href={`/litters/`}><Button className="mb-3" variant="secondary">Back to Litters</Button></a>
+                    </div>
+                    <div>
+                        <Form onSubmit={handleSubmit}>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Enter Name of Litter</Form.Label>
+                                <Form.Control type="text" placeholder="Enter Name" ref={name}
+                                    required />
+                            </Form.Group>
 
-                                <Form.Group className="mb-3" controlId="formBasicPassword">
-                                    <Form.Label>Mother:</Form.Label>
-                                    <Form.Select aria-label="Default select example" ref={father}>
-                                        {parents.map((parent) => {
-                                            return (
+                            <Form.Group className="mb-3" controlId="formChooseMother">
+                                <Form.Label>Mother:</Form.Label>
+                                <Form.Select aria-label="Default select example" ref={father}
+                                    required
+                                >
 
-                                                !parent.gender ?
-                                                    <option value={parent._id} > {parent.name}</option> : ""
+                                    {parents.map((parent) => {
+                                        return (
 
-                                            )
-                                        })}
-                                    </Form.Select>
-                                </Form.Group>
-                                <Form.Group className="mb-3" controlId="formBasicPassword">
-                                    <Form.Label>Father:</Form.Label>
-                                    <Form.Select aria-label="Default select example" ref={father}>
-                                        {parents.map((parent) => {
-                                            return (
+                                            !parent.gender ?
+                                                <option value={parent._id} > {parent.name}</option> : ""
 
-                                                parent.gender ?
-                                                    <option value={parent._id} > {parent.name}</option> : ""
+                                        )
+                                    })}
+                                </Form.Select>
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formChooseFather">
+                                <Form.Label>Father:</Form.Label>
+                                <Form.Select aria-label="Default select example" ref={father}
+                                    required>
+                                    {parents.map((parent) => {
+                                        return (
 
-                                            )
-                                        })}
-                                    </Form.Select>
-                                </Form.Group>
-                                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                                    <Form.Label>Enter Bio of Litter</Form.Label>
-                                    <Form.Control placeholder="Enter Bio" ref={bio} as="textarea" rows={3} />
-                                </Form.Group>
-                                <Form.Label>Upload Splash Image</Form.Label>
-                                <Form.Group style={{ display: 'flex' }} className="mb-3" controlId="exampleForm.fileUpload">
-                                    <Form.Control
-                                        type="file"
-                                        required
-                                        name="file"
-                                        onChange={handleFiles}
-                                        ref={splashImg}
-                                    />
-                                    <Button variant={!submitButton ? "warning" : "success"} style={{ cursor: "pointer", }} type='button' onClick={upload}>{body.img ? "Image Uploaded" : "Upload Image"}</Button>
-                                </Form.Group>
-                                <Form.Text className="text-muted">
-                                    You must submit a photo.
-                                </Form.Text>
-                                <Form.Group>
-                                    <Button variant="success" type="submit">
-                                        Create New Litter
-                                    </Button>
-                                </Form.Group>
+                                            parent.gender ?
+                                                <option value={parent._id} > {parent.name}</option> : ""
 
-                            </Form>
-                        </div>
-                        <img className="mt-5" style={{ width: "100%", borderTop: "5px black solid", borderBottom: "5px black solid" }} src="https://yolopooch.com/wp-content/uploads/2022/05/Do-Dobermans-Come-in-Different-Colors-2-1.jpg" alt="" />
-                    </Container>
-                </>
+                                        )
+                                    })}
+                                </Form.Select>
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                                <Form.Label>Enter Bio of Litter</Form.Label>
+                                <Form.Control placeholder="Enter Bio" ref={bio} as="textarea" rows={3} />
+                            </Form.Group>
+                            <Form.Label>Upload Splash Image</Form.Label>
+                            <Form.Group style={{ display: 'flex' }} className="mb-3" controlId="exampleForm.fileUpload">
+                                <Form.Control
+                                    type="file"
+                                    required
+                                    name="file"
+                                    onChange={handleFiles}
+                                    ref={splashImg}
+                                />
+                                <Button variant={!submitButton ? "warning" : "success"} style={{ cursor: "pointer", }} type='button' onClick={upload}>{body.img ? "Image Uploaded" : "Upload Image"}</Button>
+                            </Form.Group>
+                            <Form.Text className="text-muted">
+                                You must submit a photo.
+                            </Form.Text>
+                            <Form.Group>
+                                <Button variant="success" type="submit">
+                                    Create New Litter
+                                </Button>
+                            </Form.Group>
+
+                        </Form>
+                    </div>
+                    <img className="mt-5" style={{ width: "100%", borderTop: "5px black solid", borderBottom: "5px black solid" }} src="https://yolopooch.com/wp-content/uploads/2022/05/Do-Dobermans-Come-in-Different-Colors-2-1.jpg" alt="" />
+                </Container>
             </>
         )
     }
--- src/pages/Litters/EditLitter/EditLitter.js
+++ src/pages/Litters/EditLitter/EditLitter.js
@@ -1,23 +1,41 @@
 import { useNavigate, useParams } from "react-router-dom"
 import { useEffect, useState, useRef } from "react"
 import axios from "axios"
-
+import { Form, Button, Container, InputGroup, FormGroup, Image } from "react-bootstrap";
+import { uploadImage } from "../../../utilities/image-upload";
 const EditLitter = () => {
     const { id } = useParams()
     const { mom } = useParams()
+    const [submitButton, setSubmitButton] = useState(false)
+    const [heroImage, setHeroImage] = useState("")
+    const [body, setBody] = useState({ img: '' })
+    const [files, setFiles] = useState([])
     const { dad } = useParams()
     const navigate = useNavigate()
+    const [imgToggle, setImgToggle] = useState(false)
     const name = useRef()
     const father = useRef()
     const mother = useRef()
     const bio = useRef()
+    const image = useRef()
     const splashImg = useRef()
     const imgs = useRef()
     const videos = useRef()
     const [litter, setLitter] = useState({})
     const [parents, setParents] = useState([])
     const [loading, setLoading] = useState(true)
-
+    const handleFiles = (evt) => {
+        setFiles(evt.target.files)
+    }
+    const upload = async () => {
+        const formData = new FormData()
+        formData.append('file', files[0])
+        formData.append('upload_preset', 'ohtzeh46')
+        const response = await uploadImage(formData)
+        setBody({ img: response })
+        setHeroImage(response)
+        setSubmitButton(true)
+    }
 
     useEffect(() => {
         (async () => {
@@ -62,51 +80,96 @@ const EditLitter = () => {
     const loaded = () => {
         return (
             <>
-                <h1>Edit {litter.name}</h1>
-                <a href={`/litter/${litter._id}/${litter.mother._id}/${litter.father._id}`}><button>{`Back to ${litter.name}`}</button></a>
-                <form onSubmit={handleSubmit}>
-                    <p>Enter name of the Litter</p>
-                    <input defaultValue={litter.name} placeholder='Enter name' type="text" ref={name} />
-                    <p>Father:</p>
-                    <select
-                        ref={father}
-                    >
-                        <option value={litter.father} >{litter.father.name}</option>
-                        {parents.map((parent) => {
-                            return (
-                                parent.name == litter.father.name ? "" :
-                                    parent.gender ?
-                                        <option value={parent._id} > {parent.name}</option> : ""
 
-                            )
-                        })}
-                    </select>
-                    <p>Mother:</p>
-                    <select
-                        ref={mother}
-                    >
-                        <option value={litter.mother} >{litter.mother.name}</option>
-                        {parents.map((parent) => {
-                            return (
-                                parent.name == litter.mother.name ? "" :
+                <Container style={{
+                    maxWidth: "800px", display: "flex", flexDirection: "column", justifyContent: 'center', alignItems: 'center',
+                    height: "100%",
+                    overflow: 'hidden',
+                    backgroundColor: "tan"
+                }}>
+                    <img style={{ width: "100%", borderTop: "5px black solid", borderBottom: "5px black solid" }} src="https://www.bubblypet.com/wp-content/uploads/2021/05/Four-black-and-tan-Doberman-puppies-for-sale.jpg" alt="" />
+                    <div style={{ display: "flex", flexDirection: "column", justifyContent: 'space-around', alignItems: 'center' }}>
 
-                                    !parent.gender ?
-                                        <option value={parent._id} > {parent.name}</option> : ""
+                        <h1 className="mt-5">{`Edit ${litter.name}`}</h1>
+                        <a href={`/litter/${id}/${mom}/${dad}`}><Button className="mb-3" variant="secondary">{`Back to ${litter.name}`}</Button></a>
+                    </div>
+                    <div>
+                        <Form onSubmit={handleSubmit}>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Enter Name of Litter</Form.Label>
+                                <Form.Control defaultValue={litter.name} type="text" placeholder="Enter Name" ref={name}
+                                    required />
+                            </Form.Group>
 
-                            )
-                        })}
-                    </select>
-                    <p>Enter bio of the Litter</p>
-                    <textarea defaultValue={litter.bio} placeholder='Enter bio' type="text" ref={bio} />
-                    <p>Enter splash image Link</p>
-                    <input defaultValue={litter.splashImg} placeholder='Enter image link' type="text" ref={splashImg} />
-                    <p>Enter other images</p>
-                    <input defaultValue={litter.imgs} placeholder='Enter image links' type="text" ref={imgs} />
-                    <p>Enter video of the Litter</p>
-                    <input defaultValue={litter.videos} placeholder='Enter video link' type="text" ref={videos} />
-                    <input type="submit" value={`Update ${litter.name}`} />
-                </form>
-                <button onClick={() => { handleDelete(litter._id) }}>Delete {litter.name}</button>
+                            <Form.Group className="mb-3" controlId="formChooseMother">
+                                <Form.Label>Mother:</Form.Label>
+                                <Form.Select aria-label="Default select example" ref={father}
+                                    required
+                                >
+                                    <option value={litter.mother._id}>{litter.mother.name}</option>
+                                    {parents.map((parent) => {
+                                        return (
+
+                                            !parent.gender ? parent.name !== litter.mother.name ?
+                                                <>
+                                                    <option value={parent._id}> {parent.name}</option>
+                                                </> : "" : ""
+
+                                        )
+                                    })}
+                                </Form.Select>
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formChooseFather">
+                                <Form.Label>Father:</Form.Label>
+                                <Form.Select aria-label="Default select example" ref={father}
+                                    required>
+                                    <option value={litter.father._id}>{litter.father.name}</option>
+                                    {parents.map((parent) => {
+                                        return (
+
+                                            parent.gender ? parent.name !== litter.father.name ?
+                                                <>
+                                                    <option value={parent._id}> {parent.name}</option>
+                                                </> : "" : ""
+
+                                        )
+                                    })}
+                                </Form.Select>
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                                <Form.Label>Enter Bio of Litter</Form.Label>
+                                <Form.Control placeholder="Enter Bio" ref={bio} as="textarea" rows={3} />
+                            </Form.Group>
+                            {!imgToggle ?
+                                <Button onClick={() => { setImgToggle(true) }}> {`Want To Change ${litter.name}'s Image?`}</Button>
+                                :
+                                <>
+                                    <Button onClick={() => { setImgToggle(false) }}>{`Don't Want To Change ${litter.name}'s Image?`}</Button>
+                                    <br />
+                                    <Form.Label>Upload Splash Image</Form.Label>
+                                    <Form.Group style={{ display: 'flex' }} className="mb-3" controlId="exampleForm.fileUpload">
+                                        <Form.Control
+                                            type="file"
+                                            required
+                                            name="file"
+                                            onChange={handleFiles}
+                                            ref={image}
+                                        />
+                                        <Button variant={!submitButton ? "warning" : "success"} style={{ cursor: "pointer", }} type='button' onClick={upload}>{body.img ? "Image Uploaded" : "Upload Image"}</Button>
+                                    </Form.Group>
+                                </>}
+
+                            <Form.Group>
+                                <br />
+                                <Button variant="success" type="submit">
+                                    {`Update ${litter.name}`}
+                                </Button>
+                            </Form.Group>
+
+                        </Form>
+                    </div>
+                    <img className="mt-5" style={{ width: "100%", borderTop: "5px black solid", borderBottom: "5px black solid" }} src="https://yolopooch.com/wp-content/uploads/2022/05/Do-Dobermans-Come-in-Different-Colors-2-1.jpg" alt="" />
+                </Container>
             </>
         )
     }
