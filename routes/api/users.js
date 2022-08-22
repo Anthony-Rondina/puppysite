e646c81647e81c2bde3ebede4ec3a0c6aa496a19
Anthony-Rondina <anthony.rondina@icloud.com>
2022-08-22 17:43:47 -0400

    minor change

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const usersCtrl = require('../../controllers/api/userCtrl');
+const usersCtrl = require('../../controllers/api/UserCtrl');
 const ensureLoggedIn = require('../../config/ensureLoggedIn');
 
 
