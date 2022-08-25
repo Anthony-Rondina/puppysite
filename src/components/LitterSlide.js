f8631aaa0a1c150ba122675651956cc8f5e00dfe
Anthony-Rondina <anthony.rondina@icloud.com>
2022-08-25 13:27:20 -0400

    updating styling

--- src/components/LitterSlide.js
+++ src/components/LitterSlide.js
@@ -4,7 +4,7 @@ const LitterSlide = () => {
         <Container style={{ display: "flex", justifyContent: 'center', alignItems: 'center' }} >
             <Carousel style={{ maxWidth: "1150px" }}>
                 <Carousel.Item>
-                    <img style={{ maxHeight: '700px', objectFit: 'contain' }}
+                    <img style={{ minHeight: '350px', maxHeight: '700px', objectFit: 'contain' }}
                         className="d-block w-100"
                         src="https://i2-prod.irishmirror.ie/incoming/article5037461.ece/ALTERNATES/s1200b/puppies-main.jpg"
                         alt="First slide"
@@ -14,7 +14,7 @@ const LitterSlide = () => {
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img style={{ maxHeight: '700px', objectFit: 'contain' }}
+                    <img style={{ minHeight: '350px', maxHeight: '700px', objectFit: 'contain' }}
                         className="d-block w-100"
                         src="https://i2-prod.mirror.co.uk/incoming/article5037028.ece/ALTERNATES/s1227b/Rescue-dog-gives-birth-to-a-record-14-Doberman-puppies.jpg"
                         alt="Second slide"
@@ -25,7 +25,7 @@ const LitterSlide = () => {
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img style={{ maxHeight: '700px', objectFit: 'contain' }}
+                    <img style={{ minHeight: '350px', maxHeight: '700px', objectFit: 'contain' }}
                         className="d-block w-100"
                         src="https://images.squarespace-cdn.com/content/v1/5d4cff87cba6000001952085/1565712359608-AT3EHEAZIWCNT58FCJ9X/processed_20190218_171233.jpg"
                         alt="Third slide"
