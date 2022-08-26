e125b8ea527b59a8e9eaa2d1ec6789f2890dcf61
Anthony-Rondina <anthony.rondina@icloud.com>
2022-08-26 09:41:06 -0400

    adding user login

--- models/User.js
+++ models/User.js
@@ -38,6 +38,9 @@ const userSchema = new Schema({
         type: String,
         required: false,
         minLength: 1
+    },
+    admin: {
+        type: Boolean
     }
 }, {
     timestamps: true,
